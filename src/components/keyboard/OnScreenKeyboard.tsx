38cfc01a0187060562568e187e61cfe14d16e35a
MitaKita <marita_kontio@hotmail.com>
2025-09-07 18:44:14 -0300

    Keyboard width correction

--- src/components/keyboard/OnScreenKeyboard.tsx
+++ src/components/keyboard/OnScreenKeyboard.tsx
@@ -11,7 +11,7 @@ export const OnScreenKeyboard = () => {
   return (<>
     <h2 className="text-2xl font-bold text-cyan-800 text-center pb-4 pt-20">{params.type} Special Characters</h2>
     <div className="flex flex-col gap-2 items-center">
-      <div className="flex flex-row gap-2 items-center w-1/2 flex-wrap justify-center">
+      <div className="flex flex-row gap-2 items-center w-full flex-wrap justify-center">
         {keys.map((char, rowIdx) => (
           <OnScreenKeyButton key={getKey(char, rowIdx)} keyId={char} />
         ))}
